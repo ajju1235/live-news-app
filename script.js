f97c0fc72fcf87af449aae11a96a50214420791c
Arju Singh Baghel <ajjusingh00011@gmail.com>
2026-03-12 03:04:02 +0530

    fixed api error

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const API_KEY = "54ef95f8b65e437c89cc2b375eb44619";
 const url = "https://newsapi.org/v2/everything?q=";
-//   https://newsapi.org/
 
 window.addEventListener("load", () => fetchNews("India"));
 
@@ -9,10 +8,23 @@ function reload() {
 }
 
 async function fetchNews(query) {
-  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-  const data = await res.json();
-  console.log(data);
-  bindData(data.articles);
+  try {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+
+    console.log(data);
+
+    // API error check
+    if (!data.articles) {
+      console.error("No articles found");
+      return;
+    }
+
+    bindData(data.articles);
+
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
 }
 
 function bindData(articles) {
@@ -23,6 +35,7 @@ function bindData(articles) {
 
   articles.forEach((article) => {
     if (!article.urlToImage) return;
+
     const cardClone = newsCardTemplate.content.cloneNode(true);
     fillDataInCard(cardClone, article);
     cardsContainer.appendChild(cardClone);
@@ -36,25 +49,31 @@ function fillDataInCard(cardClone, article) {
   const newsDesc = cardClone.querySelector("#news-desc");
 
   newsImg.src = article.urlToImage;
-  newsTitle.innerHTML = article.title;
-  newsDesc.innerHTML = article.description;
+  newsTitle.innerText = article.title;
+  newsDesc.innerText = article.description;
 
   const date = new Date(article.publishedAt).toLocaleString("en-US", {
-    timeZone: "Asia/Jakarta",
+    timeZone: "Asia/Kolkata",
   });
 
-  newsSource.innerHTML = `${article.source.name} ${date}`;
+  newsSource.innerText = `${article.source.name} • ${date}`;
 
   cardClone.firstElementChild.addEventListener("click", () => {
     window.open(article.url, "_blank");
   });
 }
+
 let curSelectedNav = null;
+
 function onNavItemClick(id) {
   fetchNews(id);
+
   const navItem = document.getElementById(id);
+
   curSelectedNav?.classList.remove("active");
+
   curSelectedNav = navItem;
+
   curSelectedNav.classList.add("active");
 }
 
@@ -63,8 +82,12 @@ const searchText = document.getElementById("search-text");
 
 searchButton.addEventListener("click", () => {
   const query = searchText.value;
+
   if (!query) return;
+
   fetchNews(query);
+
   curSelectedNav?.classList.remove("active");
+
   curSelectedNav = null;
 });
